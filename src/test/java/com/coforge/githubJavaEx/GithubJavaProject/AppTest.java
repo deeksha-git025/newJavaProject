c7fdd0444887f2118a651bd361c670fcbb46d247
LENOVO <LENOVO@deeksha2019.mshome.net>
2021-02-12 10:55:56 +0530

    New Commit

---
+++ src/test/java/com/coforge/githubJavaEx/GithubJavaProject/AppTest.java
@@ -0,0 +1,20 @@
+package com.coforge.githubJavaEx.GithubJavaProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
